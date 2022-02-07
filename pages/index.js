6a7f9e5c89a1fea138e64ca7473477cba8fbc14b
2WBA123 <wahabadil433@gmail.com>
2022-02-07 11:32:13 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Sidebar from '../components/sidebar'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Twitter Clone</title>
+      </Head>
+
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar />
+      </main>
+
+    </div>
+  )
+}
