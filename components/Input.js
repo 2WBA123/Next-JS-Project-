ac411a4afd659610b9ae109056bac43b2608380e
Abdul Wahab adil <wahabadil433@gmail.com>
2022-03-08 16:20:40 +0500

    inputsall

--- components/Input.js
+++ components/Input.js
@@ -14,8 +14,33 @@ function Input() {
   const [input, setInput] = useState("");
   const [selected, setSelected] = useState(null);
   const filePickerRef = useRef(null);
-  const [showEmojis,setShowEmojis]=useState(false)
-  const addImageToPost = ()=>{}
+  const [showEmojis, setShowEmojis] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    console.log("pic")
+    reader.onload = (readerEvent) => {
+      setSelected(readerEvent.target.result);
+    };
+  };
+
+  const sendPost = () =>{
+      if(loading) return;
+      setLoading(true);
+  }
+
+  const addEmoji = (e) => {
+    let sym = e.unified.split("-");
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInput(input + emoji);
+  };
+
   return (
     <div className={`border-b border-gray-700 p-3 flex space-x-3 `}>
       <div>
@@ -24,7 +49,7 @@ function Input() {
       </div>
 
       <div className="w-full divide-y divide-gray-700 ">
-        <div className={``}>
+        <div className={`${selected && "pb-7"} ${input && "space-y-2.5"}`}>
           <textarea
             value={input}
             rows="2"
@@ -36,7 +61,7 @@ function Input() {
             selected && (
               <div className="relative">
                 <div className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center
-                justify-center top-1 left-1 cursor-pointer" onClick={()=>setSelected(null)}>
+                justify-center top-1 left-1 cursor-pointer" onClick={() => setSelected(null)}>
                   <XIcon className="text-white h-5" />
                 </div>
                 <img src={selected} alt="" className="rounded-2xl max-h-80 object-contain " />
@@ -45,39 +70,48 @@ function Input() {
           }
 
         </div>
-        <div className="flex items-center justify-between pt-2.5">
-              <div className="flex items-center">
-                <div className="icon" onClick={()=>filePickerRef.current.click()}>
-                   <PhotographIcon className="h-[22px] text-[#1d9bf0]"/>
-                   <input type="file" onChange={addImageToPost} hidden ref={filePickerRef}/> 
-                </div>
+        {!loading && (
+          <div className="flex items-center justify-between pt-2.5">
+          <div className="flex items-center">
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+              <input type="file" onChange={addImageToPost} hidden ref={filePickerRef} />
+            </div>
 
-                <div className="icon rotate-90">
-                <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
-              </div>
+            <div className="icon rotate-90">
+              <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
 
-              <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
-                <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
-              </div>
+            <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+              <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
 
-              <div className="icon">
-                <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
-              </div>
-               {showEmojis && (
-                  <Picker
-                  onSelect={addEmoji}
-                  style={{
-                    position: "absolute",
-                    marginTop: "465px",
-                    marginLeft: -40,
-                    maxWidth: "320px",
-                    borderRadius: "20px",
-                  }}
-                  theme="dark"
-                />
-               )}
-              </div>
+            <div className="icon">
+              <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+            {showEmojis && (
+              <Picker
+                onSelect={addEmoji}
+                style={{
+                  position: "absolute",
+                  marginTop: "465px",
+                  marginLeft: -40,
+                  maxWidth: "320px",
+                  borderRadius: "20px",
+                }}
+                theme="dark"
+              />
+            )}
+          </div>
+          <button className="bg-[#1d9bf0] text-white rounded-full
+               px-4 py-1.5 
+              font-bold shadow-md hover:bg-[#1a8cd8]
+               disabled:hover:bg-[#1d9bf0] disabled:opacity-50
+                disabled:cursor-default" disabled={!input.trim() && !selected}
+                // onClick={sendPost}
+                >Tweet</button>
         </div>
+        )}
       </div>
     </div>
   )
